25dcfdf63e8ba3216343a71fe7903f89acf49543
Q-wrld97 <dough_boy97@hotmail.com>
2023-05-23 11:29:31 -0400

    Alpha beta added

--- README.md
+++ README.md
@@ -2,6 +2,40 @@
 
 Using the Min-Max Algorithm and Alpha Beta Pruning, this project implements an AI player that plays tic-tac-toe.
 
+## Project Overview:
+  The AI Tic Tac Toe project aims to create an interactive Tic Tac Toe game where users can play against different AI players. The project implements two AI algorithms, namely Alpha-Beta Pruning and Minimax, along with a random AI player for comparison. Minimax and Alpha-beta pruning AI, the heuristic function is where the AI looks ahead to future game states and scores them. The score reflects how favorable that state is to the AI (basically the AI favor state it can win and dislike state that it lose). The main difference between the two is that Alpha-beta pruning includes a step where it may prune (ignore) branches of the game tree that it has determined will never be favorites, which can speed up the decision making process.
+
+## Design Decisions:
+  AI Player Selection: The project provides three AI player options: Alpha-Beta Pruning AI, Minimax AI, and Random AI. This allows users to choose the level of experience of different AI strategies. The Alpha-Beta Pruning AI and Minimax AI are both unbeatable, but the Alpha-Beta Pruning AI is more efficient and makes fewer calculations, leading to faster gameplay. The Random AI is a simple AI that makes random moves and is easy to beat.
+
+  Board Representation: The Tic Tac Toe game board is represented using a dictionary in Python. Each key-value pair represents a position on the game board where the key is the position on the board and the value is the current symbol ('X', 'O', or an empty space). This representation allows for easy access to the game_board positions and simplifies the implementation of the game logic. (i.e game_board[1] = 'X' represents the top left position on the game_board and game_board[9] = 'O' represents the bottom right position on the game_board). Originally the thought was to use 2D list but after some debating we decided to use dictionary because The dictionary representation simplifies the game logic by providing intuitive access to board positions using keys. It eliminates the need for nested loops or complex indexing required in a 2D list representation
+    i.e game_board[1][1] = 'X' represent top left position.
+    for row in board:
+      for position in row:
+          print(position, end=' ')
+      print()
+  This nested loop structure can become more complex and less readable as the board size increases or when performing operations that require accessing specific positions.
+  
+  Validation: The implementation include check to validate the user inputs move and ensure it is within the valid range of positions on the game_board. This ensures fair gameplay and prevents invalid moves. Early on in the project, if a player made an invalid move, the AI would still make a move and the game would continue. This was fixed by adding a check to ensure that the player's move is valid before proceeding with the AI's move. 
+
+  Error Handling: The code includes error handling to handle invalid user inputs and prevent unexpected crashes or incorrect behavior during gameplay. This includes checks for invalid user input for the move position and the AI player selection. The code also includes checks to ensure that the AI player does not make a move if the game has already ended or if user make an invalid move. This prevents the AI from making an unnecessary move after the game has ended.
+
+  Random AI Player for Comparison: The inclusion of a random AI player allows users to experience different levels of AI gameplay and understand the difference in strategies employed. This also allows users to compare the performance of the Minimax and Alpha-Beta Pruning AI players against a random AI player. The random AI player is also useful for testing the game logic and ensuring that the game does not crash or behave unexpectedly when the AI makes a move.
+    
+
+## Limitations:
+  Lack Modularity: The current implementation is procedural and lacks a modular structure. While it functions correctly, the code could benefit from an OOP approach, where game entities (such as the game game_board, players, and AI strategies) are encapsulated within classes, leading to cleaner code organization and easier scalability.
+    
+    
+## Future Improvements:
+  Game Statistics: utilizing a database to track and display previous game statistics such as win/loss/tie ratios for each player, allowing users to evaluate their performance over time.
+    
+  Object-Oriented Programming (OOP):utilize object-oriented programming principles. This can improve code organization, modularity, and maintainability, making it easier to scale and enhance the game in the future.
+    
+  AI Difficulty: Currently both alpha beta and min_max are unbeatable but a way we can improve this by implementing a random factor to the AI move this will make the AI more fun to play against and less predictable. A possible way to do this is to keep a list of best move and randomly choosing a move from such list or use mean to take the avg move favorable move.
+
+
+
 ## Report
 
 ### Date: 5/19/2023
@@ -29,11 +63,21 @@ The implementation of the game board representation was changed from a 2D list t
 Minimax AI Implementation and Renaming Variable
     
 #### Details: 
-The Minimax AI algorithm was implemented. The algorithm was implemented using a recursive approach. The algorithm was tested and verified to be working correctly. The Minimax AI was implemented by Quang Le and tested by Adam Clark by feeding AI the multiple variation of game board and checking if it chooses the correct solution.
+The Minimax AI algorithm was implemented. The algorithm was implemented using a recursive approach. At first we ran into a huge infinite recusion loop but after some more research we realize that we never had any terminal state where the recursion would exit which was added by returning the status of the board using winner_checker. The Minimax AI was implemented by Quang Le and tested by Adam Clark by feeding AI the multiple variation of game board and checking if it chooses the correct solution.
 
+### Date: 5/22/2023-5/23/2023
+
+#### Description: 
+Validation,Error Handling,Alpha-Beta Pruning AI (ExCred) Implementation and Player Choice for AI, Modularization of Code
+    
+#### Details: 
+The code was updated to include validation and error handling. This includes checks for invalid user input for the move position. The code also includes checks to ensure that the AI player does not make a move if the game has already ended. This prevents the AI from making an unnecessary move after the game has ended. The validation and error handling was implemented by Quang Le and tested by Adam Clark and Mahmood Sakib by playing the game and testing for invalid inputs and unexpected behavior.The Alpha-Beta Pruning AI algorithm was implemented. The algorithm was implemented similarly to Min-Max but now include pruning to eliminate unfavorable outcome by breaking before a score could be return. The algorithm was tested and verified to be working correctly. The Alpha-Beta Pruning AI was implemented by Quang Le and tested by Adam Clark and Mahmood Sakib by playing the game and checking if the AI makes the correct moves by comparing it to the Minimax AI moves and also comparing time it takes to make a decision. The Alpha-Beta Pruning AI was found to be more efficient and faster than the Minimax AI which is expected. Up until this point if we wanted to switch between AI players we would have to manually comment out the code for the AI we don't want to use and uncomment the code for the AI we want to use. This is not ideal so we decided to implement a way for the user to choose which AI they want to play against. This was implemented by Adam Clark and Mahmood Sakib and tested by Quang Le by playing the game and selecting different AI players and verifying that the correct AI player is selected. The final touch up for our code was breaking up the main code into smaller function making it more modular and readable. This was implemented by Adam Clark and tested by Quang Le and Mahmood Sakib by playing the game and verifying that the game still works as intended.
+
+## Resouces
+[The Coding Train](https://www.youtube.com/watch?v=trKjYdBASyQ&t=30s)
+[Anmol Chandel](https://github.com/anmolchandelCO180309/tic-tac-toe-using-alpha-beta-pruning)
 
 ## Contributors
-
 - [Mahmood Sakib](https://github.com/mSakib20)
 - [Quang Le](https://github.com/Q-Wrld97)
 - [Adam Clark](https://github.com/clarkca7)
--- TicTacToe.py
+++ TicTacToe.py
@@ -8,6 +8,7 @@ Date: 06/06/2023
 import math
 import random
 
+
 # Initialize the game game_board as a dictionary
 game_board = {1: ' ', 2: ' ', 3: ' ',
               4: ' ', 5: ' ', 6: ' ',
@@ -15,7 +16,6 @@ game_board = {1: ' ', 2: ' ', 3: ' ',
 
 # Function to display the game game_board
 def show_board(game_board):
-    # Printing the game board in a grid form using the values stored in the dictionary
     print(f" {game_board[1]} | {game_board[2]} | {game_board[3]} ")
     print("===========")
     print(f" {game_board[4]} | {game_board[5]} | {game_board[6]} ")
@@ -24,36 +24,173 @@ def show_board(game_board):
 
 # Function to check for a winner
 def winner_checker(game_board):
-    # List of all possible winning combinations
-    winning_combinations = [(1, 2, 3), (4, 5, 6), (7, 8, 9),  
-                            (1, 4, 7), (2, 5, 8), (3, 6, 9),  
-                            (1, 5, 9), (3, 5, 7)]  
+    # Define the winning combinations on the game board
+    winning_combinations = [(1, 2, 3), (4, 5, 6), (7, 8, 9),  # rows
+                            (1, 4, 7), (2, 5, 8), (3, 6, 9),  # columns
+                            (1, 5, 9), (3, 5, 7)]  # diagonals
 
-    # Check if any winning combination matches for 'X' or 'O'
+    # Check if any of the winning combinations have been achieved by a player
     for combination in winning_combinations:
         if game_board[combination[0]] == game_board[combination[1]] == game_board[combination[2]] != ' ': # If all 3 values are same and not empty
-            return game_board[combination[0]] 
+            return game_board[combination[0]]  # Return the winning player's symbol (X or O)
 
-    # If all spaces are filled and no winner, it's a tie
+    #  If all spaces are filled and no winner, it's a tie
     if ' ' not in game_board.values():
         return 'tie'
 
-    # If none of the above conditions are met, game continues
-    return None  
+    return None  # Return None if there is no winner yet
 
 # Function to get the user's move
 def user_turn(game_board):
-    # Get move from user as input, validate it and return
     while True:
         try:
-            move = int(input("Please make your move: ")) 
+            move = int(input("Please make your move: "))  # Get user input for move
             if move in range(1, 10) and game_board[move] == ' ':
-                return move  
+                return move  # Return the valid move
             else:
                 print("Invalid move please input a valid move")
         except ValueError:
             print("Invalid input. Please enter a number.")
+            
+# Function to make the AI's move using Minimax
+'''
+START
+|
+|--> 1. Start from the root (current board state)
+|    |
+|    |--> 2. Generate all possible next states
+|    |
+|    |--> 3. For each state, evaluate using the heuristic function
+|    |
+|    |--> 4. Select the move with the best heuristic value
+|    |
+|
+|---> 5. Make the move
+|
+END
+'''
+def min_max_ai_move(game_board):
+    best_score = -math.inf
+    best_move = None
+    for move in game_board.keys():
+        if game_board[move] == ' ':
+            game_board[move] = 'O'  # Make a hypothetical move for the AI
+            score = min_max(game_board, False)  # Calculate the score using the Minimax algorithm
+            game_board[move] = ' '  # Undo the hypothetical move
+            if score > best_score:
+                best_score = score
+                best_move = move
+    return best_move  # Return the best move for the AI
 
+# Minimax algorithm
+def min_max(game_board, maxing):
+    winner = winner_checker(game_board)
+    if winner == 'O':
+        return 1  # Return 1 if the AI wins
+    elif winner == 'X':
+        return -1  # Return -1 if the player wins
+    elif winner == 'tie':
+        return 0  # Return 0 if it's a tie
+
+    if maxing:
+        max_score = -math.inf
+        for move in game_board.keys():
+            if game_board[move] == ' ': # for any empty space
+                game_board[move] = 'O'  # Make a hypothetical move for the AI
+                score = min_max(game_board, False)  # Recursively call the algorithm for the player's turn
+                game_board[move] = ' '  # Undo the hypothetical move
+                max_score = max(score, max_score)  # Update the max score
+        return max_score  # Return the max score for the AI
+    else:
+        min_score = math.inf
+        for move in game_board.keys():
+            if game_board[move] == ' ': # for any empty space
+                game_board[move] = 'X'  # Make a hypothetical move for the player
+                score = min_max(game_board, True)  # Recursively call the algorithm for the AI's turn
+                game_board[move] = ' '  # Undo the hypothetical move
+                min_score = min(score, min_score)  # Update the min score
+        return min_score  # Return the min score for the player
+
+# Function to make the AI's move using Alpha-Beta Pruning
+'''
+START
+|
+|--> 1. Start from the root (current board state)
+|    |
+|    |--> 2. Generate all possible next states
+|    |
+|    |--> 3. For each state, evaluate using the heuristic function
+|    |     |
+|    |     |--> 4. If the heuristic value is less than Alpha, Prune the branch
+|    |     |
+|    |     |--> 5. Update Beta
+|    |
+|    |--> 6. Select the move with the best heuristic value
+|
+|--> 7. Make the move
+|
+END
+'''
+def alphabeta_ai_move(game_board):
+    best_score = -math.inf
+    best_move = None
+    for move in game_board.keys():
+        if game_board[move] == ' ': # for any empty space
+            game_board[move] = 'O'  # Make a hypothetical move for the AI
+            score = alphabeta(game_board, -math.inf, math.inf, False)  # Calculate the score using the Alpha-Beta algorithm
+            game_board[move] = ' '  # Undo the hypothetical move
+            if score > best_score: # if score is better than best score
+                best_score = score # Update the best score
+                best_move = move # Update the best move
+    return best_move  # Return the best move for the AI
+
+# Alpha-Beta Pruning algorithm
+def alphabeta(game_board, alpha, beta, maxing):
+    winner = winner_checker(game_board)
+    if winner == 'O':
+        return 1  # Return 1 if the AI wins
+    elif winner == 'X':
+        return -1  # Return -1 if the player wins
+    elif winner == 'tie':
+        return 0  # Return 0 if it's a tie
+
+    if maxing:
+        max_score = -math.inf
+        for move in game_board.keys():
+            if game_board[move] == ' ':
+                game_board[move] = 'O'  # Make a hypothetical move for the AI
+                score = alphabeta(game_board, alpha, beta, False)  # Recursively call the algorithm for the player's turn
+                game_board[move] = ' '  # Undo the hypothetical move
+                max_score = max(score, max_score)  # Update the max score
+                alpha = max(alpha, max_score) # Update the alpha value with the larger value
+                if beta <= alpha: # if beta is less than or equal to alpha prune the tree
+                    break
+        return max_score  # Return the max score for the AI
+    else:
+        min_score = math.inf
+        for move in game_board.keys():
+            if game_board[move] == ' ':
+                game_board[move] = 'X'  # Make a hypothetical move for the player
+                score = alphabeta(game_board, alpha, beta, True)  # Recursively call the algorithm for the AI's turn
+                game_board[move] = ' '  # Undo the hypothetical move
+                min_score = min(score, min_score)  # Update the min score
+                beta = min(beta, min_score) # Update the beta value with the smaller value
+                if beta <= alpha: # if beta is less than or equal to alpha prune the tree
+                    break
+        return min_score  # Return the min score for the player
+
+# Function to make the AI's move randomly
+'''
+START
+|
+|--> 1. Generate all possible moves
+|
+|--> 2. Select a random move
+|
+|--> 3. Make the move
+|
+END
+'''
 def random_ai(game_board):
     # Create a list to store available moves
     available_moves = []
@@ -67,66 +204,38 @@ def random_ai(game_board):
 
     # Use random.choice() to select a random move from the available moves
     return random.choice(available_moves)
+  
+# Function to ask the player if they want to play another round
+def ask_play_again():
+    while True:
+        play_again = input("Do you want to play another round? Y or N: ")
+        if play_again.lower() == 'y':
+            return True
+        elif play_again.lower() == 'n':
+            return False
+        else:
+            print("Invalid choice. Please enter 'Y' or 'N'.")
             
-# Function to make the AI's move using Minimax
-def min_max_ai_move(game_board):
-    # Initialize best_score to a very small value
-    best_score = -math.inf
-    best_move = None
-    for move in game_board.keys():
-        if game_board[move] == ' ':
-            game_board[move] = 'O'  # Temporarily make a move for 'O'
-            score = min_max(game_board, False)  # Calculate score for that move
-            game_board[move] = ' '  # Undo the move
-            # If this score is better than best_score, update best_score and best_move
-            if score > best_score:
-                best_score = score
-                best_move = move
-    return best_move  
-
-# Minimax algorithm
-def min_max(game_board, is_maximizing):
-    # Get the current game status
-    winner = winner_checker(game_board)
-    # Assign scores if the game is over, based on who has won
-    if winner == 'O':
-        return 1  
-    elif winner == 'X':
-        return -1  
-    elif winner == 'tie':
-        return 0  
-
-    if is_maximizing:  # If this is the maximizing player's turn
-        max_score = -math.inf
-        for move in game_board.keys():
-            if game_board[move] == ' ':
-                game_board[move] = 'O'  # Temporarily make a move
-                score = min_max(game_board, False)  # Calculate score for that move
-                game_board[move] = ' '  # Undo the move
-                # If this score is better than max_score, update max_score
-                if score > max_score:
-                    max_score = score
-        return max_score  
-    else:  # If this is the minimizing player's turn
-        min_score = math.inf
-        for move in game_board.keys():
-            if game_board[move] == ' ':
-                game_board[move] = 'X'  # Temporarily make a move
-                score = min_max(game_board, True)  # Calculate score for that move
-                game_board[move] = ' '  # Undo the move
-                # If this score is less than min_score, update min_score
-                if score < min_score:
-                    min_score = score
-        return min_score 
+# Function to ask the player for AI player selection
+def get_ai_choice():
+    while True:
+        try:
+            ai_choice = int(input("Choose your AI opponent: "))
+            if ai_choice in [1, 2, 3]:
+                return ai_choice
+            else:
+                #if choice is not a number between 1 and 3 then print error message and ask again
+                print("Invalid choice. Please enter a number between 1 and 3.")
+        #if choice is not a number then print error message and ask again
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
 # Function to play the game
 def play():
-    #reset the game board
-    game_board = {1: ' ', 2: ' ', 3: ' ',
-                  4: ' ', 5: ' ', 6: ' ', 
-                  7: ' ', 8: ' ', 9: ' '}
-    print(
-          "Welcome to Tic Tac Toe You Will Be VS Min-Max AI \n"
+    while True:
+        # Select the AI player
+        print(
+          "Welcome to Tic Tac Toe You Will Be VS 3 type of AI (your choice) may the force be with you :)!\n"
           "========================================================\n"
           "Game Rules And Instruction:\n"
           "Enter a number from 1-9 to make a move. The number corresponds to the position on the board as shown below.\n"
@@ -136,9 +245,28 @@ def play():
           "===========\n"
           "7 | 8 | 9\n"
           "You will be playing first and your move is X and AI move will be O.\n"
-          
+          "========================================================\n"
+          "Select the AI player to play against:\n"
+          "1. AI with Alpha-Beta Pruning\n"
+          "2. AI with Minimax\n"
+          "3. Random AI"
           )
-    print("""
+        ai_choice = get_ai_choice()
+        
+        # Reset the game_board for a new game
+        game_board = {1: ' ', 2: ' ', 3: ' ', 
+                      4: ' ', 5: ' ', 6: ' ',
+                      7: ' ', 8: ' ', 9: ' '}
+
+        # Determine the AI move function based on the user's choice
+        if ai_choice == 1:
+            ai_move_function = alphabeta_ai_move
+        elif ai_choice == 2:
+            ai_move_function = min_max_ai_move
+        else:
+            ai_move_function = random_ai
+            
+        print("""
           Yoda: may the force be with you and good luck young padawan!
        __.-._
        '-._"7'
@@ -147,41 +275,54 @@ def play():
         |_)_/
         /  '-. \n
         """)
-    while True:
-        show_board(game_board)
-        move = user_turn(game_board)
-        game_board[move] = 'X'
-        winner = winner_checker(game_board)
-        if winner:
-            show_board(game_board)
-            if winner == 'X':
-                print('You won!')
-            elif winner == 'O':
-                print('AI won!')
-                print('AI: That was too ez')
-            else:
-                print('Its a tie!')
-            break
 
-        
-        move = min_max_ai_move(game_board)
-        game_board[move] = 'O'
-        winner = winner_checker(game_board)
-        if winner:
+        # Game loop
+        while True:
+            # Display the current game board
             show_board(game_board)
-            if winner == 'X':
-                print('You won!')
-            elif winner == 'O':
-                print('AI won!')
-                print('AI: That was too ez')
-            else:
-                print('Its a tie!')
-            break 
-            
-if __name__ == "__main__":
-    #play again loop
-    while True:
-        play()
-        play_again = input("Do you want to play another round? Y or N: ").lower()
-        if play_again not in ['y', 'yes']:
+
+            # Get the player's move
+            player_move = user_turn(game_board)
+            game_board[player_move] = 'X'  # Make the player's move
+
+            # Check for a winner or a tie
+            winner = winner_checker(game_board) 
+            if winner:
+                show_board(game_board)
+                if winner == 'tie':
+                    print("It's a tie\n")
+                elif winner == 'X':
+                    print("====================")
+                    print("You won\n")
+                else:
+                    print("====================")
+                    print("AI won!\n")
+                break
+
+            # Get the AI's move
+            ai_move = ai_move_function(game_board)
+            game_board[ai_move] = 'O'  # Make the AI's move this is dependent on the earlier choice of AI
+
+            # Check for a winner or a tie
+            winner = winner_checker(game_board)
+            if winner:
+                show_board(game_board)
+                if winner == 'tie':
+                    print("It's a tie\n")
+                elif winner == 'X':
+                    print("====================")
+                    print("You won\n")
+                else:
+                    print("====================")
+                    print("AI won\n")
+                break
+
+        # Ask the player if they want to play another round
+        if ask_play_again() == False:
+            print("Thank you for playing!\n")
             break
+        print("\n")
+        
+# Main function
+if __name__ == "__main__":
+    play()
